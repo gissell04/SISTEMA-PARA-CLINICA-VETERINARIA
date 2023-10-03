cbd9ff9d8eb35d24b17504ce8a70fb74795138e9
gissell04 <100855643+gissell04@users.noreply.github.com>
2023-10-03 15:42:15 -0600

    Add files via upload

---
+++ medicamento.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Medicamento</title>
+
+    <link rel="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="estilo.css">
+
+
+</head>
+<body>
+    
+
+
+    <form method ="post" autocomplete="off">
+     <img src="img/logo.svg" alt="">
+     
+     
+    <div class="input-group">
+
+        <div class="input-container"> 
+            <input type="text" name="name" placeholder="Nombre">
+            
+    </div> 
+
+    <div class="input-container"> 
+        <input type="descripcion  " name= "descripcion"   placeholder="Descripcion">
+        
+
+    </div>
+
+    <div class="input-container"> 
+        <input type="concentracion  " name= "concentracion"   placeholder="Concentracion">
+        
+
+    </div>
+    <div class="input-container"> 
+        <input type="cantidad " name= "cantidad"   placeholder="Cantidad">
+        
+
+    </div>
+
+    <div class="input-container"> 
+        <input type="unidad de medida " name= "unidad de medida"   placeholder="Unidad de medida">
+        
+
+    </div>
+
+    
+    <input name="send" type="submit" class="btn" value="Enviar">
+</div>
+</form>
+
+</body>
+</html>
